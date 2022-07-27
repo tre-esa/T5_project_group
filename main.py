ad0b3ff09ae1c000f4c2466677e4426914e31aa7
deeonnnn <109775478+deeonnnn@users.noreply.github.com>
2022-07-27 17:15:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import api, cash_on_hand_, overheads, profit_loss
+
+def main():
+  forex = api.api_function()
+  overheads.overhead_function(forex)
+  coh.coh_function(forex)
+  profit_loss.profitloss_functions(forex)
+
+ main()
