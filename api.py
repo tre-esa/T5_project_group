c1488607bac42986487d175aa1300b311907638a
tre-esa <109326136+tre-esa@users.noreply.github.com>
2022-07-27 17:05:23 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,10 @@
+url = 'https://www.alphavantage.co/query?function=WEEKLY&from_symbol=EUR&to_symbol=USD&apikey=2R52NNTGYAJTUAM3'
+r = requests.get(url)
+data = r.json()
 
+print(data)
+
+to_symbol=USD
+url = 'https://www.alphavantage.co/query?to_symbol=USD&from_symbol=EUR&to_symbol=USD&apikey=2R52NNTGYAJTUAM3'
+r = requests.get(url)
+data = r.json()
